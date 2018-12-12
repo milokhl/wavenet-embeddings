6f80dc3f42c89b8db4a6efc39e9efecd61e6d36f
Milo Knowles <mknowles@mit.edu>
2018-12-12 11:16:54 -0500

    Remove scripts

--- scripts/nsynth_generate_with_embeddings_example.sh
+++
@@ -1,9 +0,0 @@
-# Generate audio samples with precomputed embedddings (i.e from interpolation).
-# TODO: where should embeddings be?
-
-nsynth_generate \
---checkpoint_path=${WAVENET_CKPT_PATH} \
---source_path=${INPUT_AUDIO_PATH} \
---save_path=${GEN_AUDIO_PATH}/examples/ \
---encodings=true \
---batch_size=4
